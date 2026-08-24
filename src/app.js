183b3de643f88d3c8b041ce8f3f287239e4f9ec0
david <codiladavid@yahoo.com.au>
2026-08-25 02:39:24 +1000

    remove logger class and replace with functions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import { getNewUser } from './consoleReader.js';
 import { calculateBorrowingPower } from './borrowingCalculator.js';
-import { Logger } from "./logger.js";
+import { intro, summary } from "./logger.js";
 
 async function start() {
-    Logger.intro();
+    intro();
 
     const user = await getNewUser();    
     const loanResult = calculateBorrowingPower(user);
 
-    Logger.summary(loanResult);
+    summary(loanResult);
 }
 
 start();
--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,12 @@
 import { INTEREST_RATE } from './constants.js';
 
-export class Logger {
+export function intro() {
+    console.log("Mortgage Borrowing Power Calculator");
+    console.log("===================================");
+}
 
-    static intro() {
-        console.log("Mortgage Borrowing Power Calculator");
-        console.log("===================================");
-    }
-
-    static summary(loanResult) {
-        console.log("\n--- Calculation Summary ---");
-        console.log(`Maximum Borrowing Power at ${INTEREST_RATE}%: $${loanResult.maxLoanAmount.toLocaleString()}`);
-        console.log(`Assumed Monthly Mortgage Repayment: $${loanResult.monthlyRepayment.toLocaleString()} over 30 years`);
-    }
-
+export function summary(loanResult) {
+    console.log("\n--- Calculation Summary ---");
+    console.log(`Maximum Borrowing Power at ${INTEREST_RATE}%: $${loanResult.maxLoanAmount.toLocaleString()}`);
+    console.log(`Assumed Monthly Mortgage Repayment: $${loanResult.monthlyRepayment.toLocaleString()} over 30 years`);
 }
