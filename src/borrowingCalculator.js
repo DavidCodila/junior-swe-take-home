a89f920602f4448cfb35dde63f5bf94b444b455b
david <codiladavid@yahoo.com.au>
2026-08-28 11:59:46 +1000

    extract getTax api to seperate file

--- src/borrowingCalculator.js
+++ src/borrowingCalculator.js
@@ -10,43 +10,7 @@
  */
 
 import { INTEREST_RATE } from './constants.js';
-
-// Legacy placeholder functions to replace with API calls
-function getTax(income) {
-    return new Promise((resolve) => {
-    fetch("http://localhost:3000/api/tax?income=" + income, {
-            method: 'GET',
-            withCredentials: true,
-            credentials: 'include',
-            headers: {
-                'Authorization': "Bearer pat_abcdefghijklmnopqrstuvwxyz0123456789"
-            }
-        }).then(responseJson => {
-            const reader = responseJson.body.getReader();
-            return new ReadableStream({
-            start(controller) {
-                function push() {
-                reader.read().then(({ done, value }) => {
-                    if (done) {
-                    controller.close();
-                    return;
-                    }
-                    controller.enqueue(value);
-                    push();
-                });
-                }
-                push();
-            }
-            });
-        })
-        .then(stream => new Response(stream))
-        .then(response => response.json())
-        .then(text => resolve(text.tax)) 
-        .catch(error => {
-            throw new Error("Something bad happened: " + error)
-        });
-    })
-}
+import { getTax } from './api.js';
 
 function getHEM(income, dependents) {
     // REPLACE THIS
