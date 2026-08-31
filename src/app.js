73bfe45092495bb742e1d6206cf979775c2a8f62
david <codiladavid@yahoo.com.au>
2026-08-31 17:02:54 +1000

    clean up app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import { getNewUser } from './io/reader.js';
 import { calculateBorrowingPower } from './calculator/borrowingCalculator.js';
 import { intro, summary } from "./io/writer.js";
 
-async function start() {
+function start() {
     intro();
-
     getNewUser()
     .then(user => calculateBorrowingPower(user))  
     .then(loanResult => summary(loanResult))
